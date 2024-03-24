96ebf58d44a9d60cc25bc192014c39d5b4c420c5
Francisco Valdés <ecodisonante@gmail.com>
2024-03-24 11:56:17 -0300

    first commit

---
+++ src/main/java/com/fullstack/mascotas/MascotasApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/mascotas/MascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
